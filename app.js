9feb1b95ba684fb249087bdb2c23411d4e056ccc
Alisha <atayl16@yahoo.com>
2020-04-04 13:25:36 +0000

    updated DB

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express"),
 	app = express(),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://atayl16:12121430@alisha-fjrqz.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
