d9ea35640d23198fd54dadbed02a57fdb5f7235d
Alisha <atayl16@yahoo.com>
2020-04-04 13:39:26 +0000

    updated dbURL

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require("express"),
 	app = express(),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://atayl16:12121430@alisha-fjrqz.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://atayl16:12121430@alisha-fjrqz.mongodb.net/test?retryWrites=true&w=majority");
+// mongoose.connect("mongodb+srv://atayl16:12121430@alisha-fjrqz.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
